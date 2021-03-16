138184287f5b54001869c804226c73a6ef0353b9
Matthias Giger <i@matthiasgiger.com>
2021-03-16 11:34:33 +0100

    refactor(update): upgrade skip-local-postinstall package

--- installation.js
+++ installation.js
@@ -1,4 +1,4 @@
-import skip from 'skip-local-postinstall'
+import { skip } from 'skip-local-postinstall'
 import { log } from './utility/log.js'
 import { writeConfiguration } from './utility/configuration.js'
 
